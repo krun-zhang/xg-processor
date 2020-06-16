c175f59c99980d4fa88cdc429265fd6dc6057f30
张健焜 <krunzhang@gmail.com>
2020-06-16 15:12:51 +0800

    feat: 增加模板渲染器
    负责获取指定目录下的所有 groovy 脚本文件
    负责加载脚本文件并编译为 class 文件后缓存
    负责执行系列脚本文件

---
+++ src/main/java/dev/krun/xg/TemplateRender.java
@@ -0,0 +1,57 @@
+package dev.krun.xg;
+
+import groovy.lang.GroovyClassLoader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * 模板渲染器
+ * @author krun
+ * @since 2020/06/16
+ */
+public class TemplateRender {
+
+	/**
+	 * 模板文件存放目录
+	 */
+	private final String templatePath;
+
+	private List<Template> templates;
+
+	public TemplateRender(String path) {
+		templatePath = path;
+		templates = new ArrayList<>();
+	}
+
+	public static TemplateRender withEnv(Map<String, String> env) {
+		return new TemplateRender(env.getOrDefault("xg-template", "./src/main/resources/xg"));
+	}
+
+	protected List<Template> loadTemplates() {
+		File path = new File(templatePath);
+		if (!path.exists() || !path.isDirectory()) {
+			throw new UnsupportedOperationException("请提供存放模板文件的有效路径!");
+		}
+		List<File> files = Arrays.asList(Optional.ofNullable(path.listFiles((dir, name) -> name.endsWith("groovy"))).orElse(new File[0]));
+		GroovyClassLoader groovyClassLoader = new GroovyClassLoader(this.getClass().getClassLoader());
+		files.forEach(file -> {
+			System.out.println("Found template: " + file.getAbsolutePath());
+			try {
+				templates.add((Template) groovyClassLoader.parseClass(file).newInstance());
+			} catch (IOException | IllegalAccessException | InstantiationException e) {
+				e.printStackTrace();
+			}
+		});
+		return templates;
+	}
+
+	public void renderAllTemplates(String entityClassName) {
+		templates.forEach(template -> renderTemplate(template, entityClassName));
+	}
+
+	protected String renderTemplate(Template template, String entityClassName) {
+		return template.generate(entityClassName, entityClassName.substring(entityClassName.lastIndexOf(".") + 1));
+	}
+}
