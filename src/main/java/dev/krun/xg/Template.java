a393a4d190098eeb36001983bac9c827c8ccd5d3
张健焜 <krunzhang@gmail.com>
2020-06-16 15:10:48 +0800

    feat: 提供一个抽象模板类，groovy 脚本中继承它以获得实体类名

---
+++ src/main/java/dev/krun/xg/Template.java
@@ -0,0 +1,12 @@
+package dev.krun.xg;
+
+/**
+ * 模板
+ * @author krun
+ * @since 2020/06/16
+ */
+public abstract class Template {
+
+	public abstract String generate(String entityClassName, String entityName);
+
+}
